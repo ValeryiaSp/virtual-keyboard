568e2324f9f18dee7b47b3388a5cb2bb0acadeae
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2023-04-30 12:57:55 +0300

    feat: add styles for elements

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let body = document.createElement('body');
 let inputField = document.createElement('textarea');
+inputField.setAttribute("class" , "textarea");
+inputField.setAttribute("cols" , "45");
 let keyBoardField = document.createElement('div');
 keyBoardField.setAttribute("class" , "keyboard");
-inputField.setAttribute("class" , "printing-area")
+
 let arrOfKeys = [
   [{eventCode: '' , isFunctional : '', dataKey : ''}],
   [{eventCode: '' , isFunctional : '', dataKey : ''}],
