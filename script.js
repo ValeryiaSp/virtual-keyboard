1da396dcde66820970d9186c92092d134bd324f5
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2023-04-30 10:23:44 +0300

    feat: add pure stuctural parts of the task

---
+++ script.js
@@ -0,0 +1,12 @@
+let body = document.createElement('body');
+let inputField = document.createElement('textarea');
+let keyBoardField = document.createElement('div');
+keyBoardField.setAttribute("class" , "keyboard");
+inputField.setAttribute("class" , "printing-area")
+let arrOfKeys = [
+  [{eventCode: '' , isFunctional : '', dataKey : ''}],
+  [{eventCode: '' , isFunctional : '', dataKey : ''}],
+  [{eventCode: '' , isFunctional : '', dataKey : ''}],
+  [{eventCode: '' , isFunctional : '', dataKey : ''}],
+  [{eventCode: '' , isFunctional : '', dataKey : ''}],
+];
